0460c374c75afc78169e04f9f1984e7fbca8465a
MatthieuMeo <matthieu.simeoni@gmail.com>
2021-03-04 14:47:46 +0100

    Updated documentation for pycsou-gsp

--- pycsou-gsp/html/api/tessellations/index-1.py
+++ pycsou-gsp/html/api/tessellations/index-1.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pycsou_gsp.tessellation import cvxhull_graph
+from pycsou_gsp.graph import cvxhull_graph
 from pygsp.plotting import plot_graph
 theta, phi = np.linspace(0,np.pi,6, endpoint=False)[1:], np.linspace(0,2*np.pi,9, endpoint=False)
 theta, phi = np.meshgrid(theta, phi)
